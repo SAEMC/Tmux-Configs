d1a2ad85b193e5ebb5bbb4418d70b1af7d36a90e
SAEMC <saemc27@gmail.com>
2022-07-20 01:27:54 +0900

    Mod

--- tmux_settings.sh
+++ tmux_settings.sh
@@ -170,7 +170,7 @@ set-option -g repeat-time 3000
 set-option -g set-titles on
 set-window-option -g automatic-rename on
 set-window-option -g window-status-activity-style fg=red
-set-window-option -g window-status-current-style fg=green
+set-window-option -g window-status-current-style fg=green,bold
 
 unbind C-b
 unbind -T copy-mode-vi Space
